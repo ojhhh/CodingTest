279f3450424b9669e09d8a03ebc70c26a7414545
ojhhh <winnel@naver.com>
2023-10-05 20:47:42 +0900

    programmers coding test

---
+++ programmers/javascript/lv0/배열 원소의 길이.js
@@ -0,0 +1,17 @@
+// 배열 원소의 길이
+// 문제 설명
+// 문자열 배열 strlist가 매개변수로 주어집니다. strlist 각 원소의 길이를 담은 배열을 retrun하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// 1 ≤ strlist 원소의 길이 ≤ 100
+// strlist는 알파벳 소문자, 대문자, 특수문자로 구성되어 있습니다.
+// 입출력 예
+// strlist	result
+// ["We", "are", "the", "world!"]	[2, 3, 3, 6]
+// ["I", "Love", "Programmers."]	[1, 4, 12]
+
+function solution(strlist) {
+  let answer = [];
+  strlist.forEach((a) => answer.push(a.length));
+  return answer;
+}
